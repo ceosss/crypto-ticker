217754b0a1d26649e77967b04db9d3745048f9af
ceosss <sswarajsamant@gmail.com>
2021-07-15 10:27:08 +0530

    NEW: Basic Structure

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
 
 func main() {
 	fmt.Println("Crpyto-ticker")
+	reader := bufio.NewReader(os.Stdin)
+	getData(reader)
+}
+
+func getData(reader *bufio.Reader) {
+	text, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal("An error occurred, " + err.Error())
+	}
+	text = strings.Replace(text, "\n", "", -1)
+	println(text)
 }
